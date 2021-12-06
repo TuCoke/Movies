cb2d978a5a4db95643a6bdea2a1a4d5175a1b604
huipu <2764136779@qq.com>
2021-12-07 07:49:16 +0800

    insert

--- fans/fans/pipelines.py
+++ fans/fans/pipelines.py
@@ -5,15 +5,21 @@
 
 
 # useful for handling different item types with a single interface
+from fans.Sqlhelper.hepler import DBHelper
 
 
 class FansPipeline:
     def process_item(self, item, spider):
+        _mysql = DBHelper()
         _exit_sql = 'SELECT request_id FROM Movies m WHERE request_id =%s'
         _exit_age = (item['request_id'])
 
         _insert_sql = "INSERT INTO `Movies`" \
-                      "(Title, Tags, request_id, del_url, next_url, subtitle, cover, created_at, Links)" \
-                      " VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s)"
-
+                      "(Title, Tags, request_id, del_url, next_url, subtitle, cover, created_at, Links,Context)" \
+                      " VALUES(%s, %s, %s, %s, %s, %s, %s, %s, %s,%s)"
+        _insert_age = (item['title'], 1, item['request_id'],
+                       item['insert_del'], item['subtitle'], item['cover'], "null", "null", 'null', 'null'
+                       )
+        _result = _mysql.insert(_insert_sql, _insert_age)
+        print(f"_result", _result)
         return item
--- fans/fans/spiders/yysdfan.py
+++ fans/fans/spiders/yysdfan.py
@@ -17,8 +17,10 @@ class YysdfanSpider(scrapy.Spider):
     del_data = {
         "id": ""
     }
-    header = {'Content-Type': 'application/json'}
-    _exit = bool
+    header = {'Content-Type': 'application/json',
+              'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.55 Safari/537.36 Edg/96.0.1054.41',
+              'origin': 'https://yyds.fans'
+              }
 
     def start_requests(self):
         print(f"请求了")
@@ -26,47 +28,41 @@ class YysdfanSpider(scrapy.Spider):
             yield scrapy.FormRequest(url=url, formdata=self.data, callback=self.parse)
 
     def parse(self, response):
+        data = response.json()
         print(f"data", response.json())
-
+        print(f"data状态", data['status_code'])
         urls = 'https://cmn.yyds.fans/api/posts'
-
-        # self.data['skip'] = 30
-        # print(f"data", json.dumps(self.data))
-        # yield Request(url=urls, method='POST', body=json.dumps(self.data), headers=self.header,
-        #               callback=self.parse_json)
-
         # 翻页
-
-        page_curr = 1
-        while self._exit is bool:
-
-            if self._exit is True:
-                _exit = True
-                print(f"第{page_curr}")
-                page_curr += 1
-                self.data['skip'] = page_curr
-                print(f"_exit的值", self._exit)
-                print(f"data", json.dumps(self.data))
+        # page_curr = 1
+        # print(f"第{page_curr}")
+        # page_curr += 1
+        # self.data['skip'] = page_curr
+        # print(f"data", json.dumps(self.data))
+        page = 2
+        while True:
+            page += 1
+            print('*' * 50)
+            # print("正在爬取第 %s 页" % page)
+            if page < 999:
                 yield Request(url=urls, method='POST', body=json.dumps(self.data), headers=self.header,
                               callback=self.parse_json)
             else:
+                # print("结束 %s 页" % page)
                 break
 
     # 翻页的json
     def parse_json(self, response):
         _data = response.json()
-        print(f"_data", _data)
-        if not _data['data'] is None:
+        print(f"_data", _data['status_code'])
+        if _data['status_code'] == 429:
             print(f"数据为null")
-            self._exit = False  # 没数据不在翻页
         else:
             for _node in _data['data']:
                 item = FansItem()
                 item['request_id'] = _node['id']
                 item['title'] = _node['title']
-                # _tags = [_node['tags'][tag] for tag in range(len(_node['tags']))
-                #          if not len(_node['tags'][tag]) == 0]
-
+                item['cover'] = _node['cover']
+                item['subtitle'] = _node['subtitle']
                 if not len(_node['tags']) == 0:
                     # print(f"tags长度", len(_node['tags']))
                     # print(f"tags", _node['tags'])
@@ -90,15 +86,20 @@ class YysdfanSpider(scrapy.Spider):
                     item['tags'] = _ids
                     item['tag_title'] = _titles
                     item['del_url'] = "https://cmn.yyds.fans/api/post-info/"
+                    # print("https://cmn.yyds.fans/api/post-info/%s" % _node['id'])
+                    item['insert_del'] = "https://cmn.yyds.fans/api/post-info/%s" % _node['id']
                     self.del_data['id'] = _node['id']
-                    yield Request(url=item['del_url'], method='POST', meta={'item': item}, body=json.dumps(self.del_data),
+                    yield Request(url=item['del_url'], method='POST', meta={'item': item},
+                                  body=json.dumps(self.del_data),
                                   headers=self.header,
                                   callback=self.parse_detail)
-                    time.sleep(2)
                 else:
                     print(f"tags为null")
 
-    def parse_del(self, response):
+    def parse_detail(self, response):
         item = response.meta["item"]
         print(f"详情", response.json())
+        _data = response.json()
+        # item['content'] = _data['data']['content']
+        _links = _data['data']['links']
         yield item
